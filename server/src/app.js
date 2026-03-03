5cf0f4c3a45e18b56ccf31e3be9063a66943118a
Sainath Kamble <sainathio@Sainaths-MacBook-Air.local>
2026-03-03 20:43:42 +0530

    feat(initila commit): new f1 project main repo

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+app.use('/v1/hello', (req, res) => {
+    res.status(200).json({message: "Hello World!"})
+})
+
+export { app };
